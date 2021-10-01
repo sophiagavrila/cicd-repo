193788fa87bec3bf93f75f6e7ddef5d49379227b
sophiagavrila <msophiagavrila@gmail.com>
2021-10-01 09:02:25 -0400

    test this merge

--- src/main/java/com/revature/controller/IndexController.java
+++ src/main/java/com/revature/controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-	
+	// just adding a comment
 	@RequestMapping("/")
 	public String index() {
 		return "index";
